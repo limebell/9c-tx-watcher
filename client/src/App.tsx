1484d7f7f5d8768ec5c47fad15afe4709f6ca253
Junhyuck Ko <mrbartrns@naver.com>
2022-01-07 15:38:24 +0900

    work: create  routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import TestPage from "./components/pages/TestPage";
 
 function App() {
   return (
     <div className="App">
-      <TestPage />
+      <Routes>
+        <Route path="/*" element={<TestPage />} />
+      </Routes>
     </div>
   );
 }
