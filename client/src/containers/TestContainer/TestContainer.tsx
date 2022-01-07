a0f2804d6bf9a46f836542026dcb260019edb17f
Junhyuck Ko <mrbartrns@naver.com>
2022-01-07 16:19:42 +0900

    work: test setInterval

--- client/src/containers/TestContainer/TestContainer.tsx
+++ client/src/containers/TestContainer/TestContainer.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function TestContainer(): JSX.Element {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prev: number) => {
+        return prev + 1;
+      });
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <div>
-      <h1>Test</h1>
+      <h1>This is Count Test: {count}</h1>
     </div>
   );
 }
