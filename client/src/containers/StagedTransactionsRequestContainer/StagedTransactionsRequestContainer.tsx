0c556d3886366dadbcffbc09fc27791d78fb0f43
Junhyuck Ko <mrbartrns@naver.com>
2022-01-10 15:38:39 +0900

    work: create frontend app
    work: install bootstrap
    
    fix: error when get stauts ok but undefined
    
    work: create frontend app
    
    work: optimize interval variables
    
    fix: commit mockdb
    
    fix: commit mockdb

--- client/src/containers/StagedTransactionsRequestContainer/StagedTransactionsRequestContainer.tsx
+++ client/src/containers/StagedTransactionsRequestContainer/StagedTransactionsRequestContainer.tsx
@@ -1,37 +1,42 @@
 import React, { useEffect, useCallback, useState } from "react";
 import apiRequest from "../../utils/api-request";
+import { StagedTransactions } from "../../types";
+import StagedTransactionsView from "../../components/organisms/StagedTransactionsView";
+import axios from "axios";
 
 function StagedTransactionsRequestContainer(): JSX.Element {
-  const [stagedTransactions, setStagedTransactions] = useState<Array<object>>(
-    []
-  );
+  const [stagedTransactions, setStagedTransactions] = useState<
+    Array<StagedTransactions>
+  >([]);
+  const [fetchSuccess, setFetchSuccess] = useState<boolean>(false);
+
   const fetchStagedTransactionData = useCallback(async () => {
     try {
       const response = await apiRequest.getTransactions();
-      setStagedTransactions(() => {
-        return [...response.data.stagedTransactions];
-      });
-      console.log(response.data);
+      setStagedTransactions(response.data.stagedTransactions);
+      setFetchSuccess(true);
       return response.data;
     } catch (error) {
-      console.error(error);
+      return;
     }
   }, []);
   useEffect(() => {
+    const source = axios.CancelToken.source();
     let mounted = true;
     if (mounted) {
-      const interval = setInterval(fetchStagedTransactionData, 1000);
+      setInterval(fetchStagedTransactionData, 1000);
     }
+
     return () => {
       mounted = false;
+      source.cancel();
     };
   }, [fetchStagedTransactionData]);
   return (
-    // Temporary DOM
-    // TODO: create organism component and connect to container
-    <div>
-      <h1>StagedTransactionsContainer</h1>
-    </div>
+    <StagedTransactionsView
+      stagedTransactions={stagedTransactions}
+      fetchSuccess={fetchSuccess}
+    />
   );
 }
 
