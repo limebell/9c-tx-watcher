6835d4a0947f05ebab2d15b47304f96cea6e467d
Junhyuck Ko <mrbartrns@naver.com>
2022-01-10 14:04:54 +0900

    work: create address view

--- client/src/containers/AddressContainer/AddressContainer.tsx
+++ client/src/containers/AddressContainer/AddressContainer.tsx
@@ -1,8 +1,14 @@
 import React, { useState, useEffect, useCallback } from "react";
+import AddressesView from "../../components/organisms/AddressesView/AddressesView";
 import apiRequest from "../../utils/api-request";
+import { Address } from "../../types";
 
 function AddressContainer(): JSX.Element {
-  const [addresses, setAddresses] = useState<object>({});
+  const [addresses, setAddresses] = useState<Address>({
+    sourceAddress: "",
+    targetAddress: "",
+  });
+  const [fetchSuccess, setFetchSuccess] = useState<boolean>(false);
   const fetchAddresses = useCallback(async () => {
     try {
       const response = await apiRequest.getAddresses();
@@ -13,8 +19,10 @@ function AddressContainer(): JSX.Element {
           targetAddress: response.data.targetAddress,
         };
       });
+      setFetchSuccess(true);
     } catch (error) {
       console.error(error);
+      setFetchSuccess(false);
     }
   }, []);
   useEffect(() => {
@@ -26,11 +34,7 @@ function AddressContainer(): JSX.Element {
       mounted = false;
     };
   }, [fetchAddresses]);
-  return (
-    <div>
-      <h1>Address Container</h1>
-    </div>
-  );
+  return <AddressesView addresses={addresses} fetchSuccess={fetchSuccess} />;
 }
 
 export default AddressContainer;
