b5bcd5ce27a7b87d4c6915bd1b335e56d879c139
Junhyuck Ko <mrbartrns@naver.com>
2022-01-10 11:07:21 +0900

    work: create api request containers

--- client/src/components/pages/HomePage/HomePage.tsx
+++ client/src/components/pages/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import NextNonceContainer from "../../../containers/NextNonceContainer";
 import StagedTransactionsRequestContainer from "../../../containers/StagedTransactionsRequestContainer";
 import PageTemplate from "../../templates/PageTemplate";
 function HomePage(): JSX.Element {
   return (
     <PageTemplate>
       <StagedTransactionsRequestContainer />
+      <NextNonceContainer />
     </PageTemplate>
   );
 }
