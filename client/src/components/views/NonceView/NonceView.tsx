0cf50ce498ab401060bbd0e91dfbe123889b6e08
Chanhyuck Ko <limeelbee@gmail.com>
2022-01-17 16:38:57 +0900

    Minor styling fix

--- client/src/components/views/NonceView/NonceView.tsx
+++ client/src/components/views/NonceView/NonceView.tsx
@@ -7,7 +7,7 @@ function NonceView(): JSX.Element {
   const { nextNonce, error }: UseNextNonceAsync = useNextNonceAsync();
   return (
     <div className="nonce-view">
-      <h3>NextNonceView</h3>
+      <h3>Next Nonce</h3>
       {error && <p>서버에 문제가 있습니다.</p>}
       <div>nextTxNonce: {nextNonce?.nextTxNonce}</div>
     </div>
